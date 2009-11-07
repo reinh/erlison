2b2a60d74b34ecd3437efd04d66582168c3a3f77
Rein Henrichs <reinh@reinh.com>
2009-11-07 13:51:22 -0800

    Working article data parsing

--- src/article.erl
+++ src/article.erl
@@ -2,16 +2,32 @@
 
 -export([parse/1]).
 
+parse(Article) when is_binary(Article) ->
+    parse(binary_to_list(Article));
 parse(Article) ->
     Lines = string:tokens(Article, "\r\n"),
-    parse_article_lines(Lines).
+    parse_lines(Lines).
 
-parse_article_lines([Line|Lines]) ->
-    parse_article_lines(Line, Lines).
-parse_article_lines([$=, $y, $b, $e, $g, $i, $n, $\s | MetaLine], Rest) ->
-    parse_data(yenc, MetaLine, Rest);
-parse_article_lines([$b, $e, $g, $i, $n, $\s | MetaLine], Rest) ->
-    parse_data(uue, MetaLine, Rest);
-parse_article_lines(_, []) ->
-    {error, no_data}.
+parse_lines([Line|Lines]) ->
+    parse_lines(Line, Lines).
+parse_lines([$=, $y, $b, $e, $g, $i, $n, $\s | Header], Rest) ->
+    parse_data(yenc, Header, Rest);
+parse_lines([$b, $e, $g, $i, $n, $\s | Header], Rest) ->
+    parse_data(uue, Header, Rest);
+parse_lines(_, []) ->
+    {error, no_data};
+parse_lines(_Line, Rest) ->
+    parse_lines(Rest).
 
+parse_data(yenc, Header, Lines) ->
+    yenc:parse_data(Header, Lines);
+parse_data(uue, Header, Lines) ->
+    uuencode:parse_data(Header, Lines).
+
+-include_lib("eunit/include/eunit.hrl").
+
+parse_test() ->
+    {ok, Article} = file:read_file("../test/data/yenc/singlepart/00000005.ntx"),
+    {ok, Expected} = file:read_file("../test/data/yenc/singlepart/testfile.txt"),
+    {ok, _, Bin} = parse(Article),
+    ?assertEqual(Bin, Expected).
--- src/yenc.erl
+++ src/yenc.erl
@@ -1,10 +1,27 @@
 -module (yenc).
 
--export ([dec/1]).
+-export ([parse_data/2,dec/1]).
 
-dec (<<$=, X, Rest/binary>>) -> <<(X-106 rem 256):8, (dec(Rest))/binary>>;
-dec (<<X, Rest/binary>>)     -> <<(X-42 rem 256):8, (dec(Rest))/binary>>;
-dec (<<>>)                   -> <<>>.
+parse_data(Header, Lines) ->
+    Meta = parse_header(Header),
+    Data = parse_body(Lines),
+    Bin  = dec(Data),
+    {ok, Meta, Bin}.
+
+parse_header(Header) -> Header.
+
+%% collect data body up to ending =yend line
+parse_body(Lines) ->
+    parse_body(Lines, []).
+parse_body([[$=, $y, $e, $n, $d | _] | _], Acc) -> lists:reverse(Acc);
+parse_body([Line|Rest], Acc) ->
+    parse_body(Rest, [Line|Acc]).
+    
+
+dec(List) when is_list(List) -> dec(list_to_binary(List));
+dec(<<$=, X, Rest/binary>>)  -> <<(X-106 rem 256):8, (dec(Rest))/binary>>;
+dec(<<X, Rest/binary>>)      -> <<(X-42 rem 256):8, (dec(Rest))/binary>>;
+dec(<<>>)                    -> <<>>.
 
 -include_lib("eunit/include/eunit.hrl").
 
