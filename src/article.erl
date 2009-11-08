fa7fff0a79694a6dade9b09483903ad94246167a
Rein Henrichs <reinh@reinh.com>
2009-11-07 17:11:30 -0800

    Add test for binary size matches size metadata value

--- src/article.erl
+++ src/article.erl
@@ -19,6 +19,8 @@ parse_yenc_test() ->
     {ok, Article} = file:read_file("../test/data/yenc/singlepart/00000005.ntx"),
     {ok, Expected} = file:read_file("../test/data/yenc/singlepart/testfile.txt"),
     {ok, Meta, Bin} = parse(binary_to_list(Article)),
+    {Size, _} = string:to_integer(proplists:get_value("size", Meta)),
     ?assertEqual(Bin, Expected),
-    ?assertEqual(Meta, [{"line", "128"}, {"size", "584"}, {"name", "testfile.txt"}]).
+    ?assertEqual(Meta, [{"line", "128"}, {"size", "584"}, {"name", "testfile.txt"}]),
+    ?assertEqual(byte_size(Bin), Size).
 
