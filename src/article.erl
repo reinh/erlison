1968ab26fd8e2cabadca875e55900ff8b69e636a
Rein Henrichs <reinh@reinh.com>
2009-11-08 02:50:18 -0800

    Add multipart support

--- src/article.erl
+++ src/article.erl
@@ -1,14 +1,19 @@
 -module(article).
 
--export([parse/1]).
+-export([download/1,parse/1]).
 
+download(ArticleID) ->
+    {ok, Article} = nntp:get_article(ArticleID),
+    parse(Article).
+
+parse(Article) when is_binary(Article) -> parse(binary_to_list(Article));
 parse(Article) ->
     Lines = string:tokens(Article, "\r\n"),
     parse_lines(Lines).
 
 parse_lines([]) -> {error, no_data};
-parse_lines(["=ybegin "++Header|Lines]) -> yenc:parse_data(Header, Lines);
-parse_lines(["begin "  ++Header|Lines]) -> uuencode:parse_data(Header, Lines);
+parse_lines(["=ybegin "++Header|Lines]) -> yenc:parse(Header, Lines);
+parse_lines(["begin "  ++Header|Lines]) -> uuencode:parse(Header, Lines);
 parse_lines([_|Lines]) -> parse_lines(Lines). 
 
 -include_lib("eunit/include/eunit.hrl").
@@ -19,5 +24,15 @@ parse_yenc_singlepart_test() ->
     {ok, Meta, Bin} = parse(binary_to_list(Article)),
     {Size, _} = string:to_integer(proplists:get_value("size", Meta)),
     ?assertEqual(Bin, Expected),
-    ?assertEqual(Meta, [{"line", "128"}, {"size", "584"}, {"name", "testfile.txt"}]),
+    ?assertEqual(Meta,
+                 [{"name", "testfile.txt"},{"line", "128"},{"size","584"}]),
     ?assertEqual(byte_size(Bin), Size).
+
+parse_yenc_multipart_test() ->
+    {ok, Article1} = file:read_file("../test/data/yenc/multipart/00000020.ntx"),
+    {ok, Article2} = file:read_file("../test/data/yenc/multipart/00000021.ntx"),
+    {ok, Expected} = file:read_file("../test/data/yenc/multipart/joystick.jpg"),
+    {ok, _, Bin1} = parse(binary_to_list(Article1)),
+    {ok, _, Bin2} = parse(binary_to_list(Article2)),
+    Bin = list_to_binary([Bin1, Bin2]),
+    ?assertEqual(Bin, Expected).
--- src/yenc.erl
+++ src/yenc.erl
@@ -1,22 +1,43 @@
 -module (yenc).
+-include_lib("records.hrl").
+-export ([parse/2,decode/1]).
 
--export ([parse_data/2,decode/1]).
-
-parse_data(Header, Lines) ->
-    Meta = parse_header(Header),
-    Data = parse_body(Lines),
+parse(Header, Lines) ->
+    {Meta, Data} = parse_data(Header, Lines),
     Bin  = decode(Data),
     {ok, Meta, Bin}.
 
+parse_data(Header, ["=ypart "++PartInfo|Lines]) ->
+    Meta = parse_header(Header),
+    PartMeta = parse_header(PartInfo),
+    Data = parse_body(Lines),
+    {lists:append(Meta, PartMeta),Data};
+parse_data(Header, Lines) ->
+    Meta = parse_header(Header),
+    Data = parse_body(Lines),
+    {Meta,Data}.
+
 parse_header(Header) ->
-    [ list_to_tuple(string:tokens(X, "=")) || X<-string:tokens(Header, " ")].
+    case string:str(Header, "name=") of
+        0 -> parse_header_without_name(Header);
+        Index ->
+            [parse_name(string:substr(Header, Index))|
+             parse_header_without_name(string:substr(Header, 1, Index-1))]
+    end.
+
+parse_header_without_name(Header) ->
+    [ list_to_tuple(string:tokens(X, "=")) || X<-string:tokens(Header, " ")].   
+parse_name(Name) ->
+    list_to_tuple(string:tokens(string:strip(Name), "=")).
+    
+
 
 %% collect data body up to ending =yend line
-parse_body(Lines) ->
-    parse_body(Lines, []).
-parse_body(["=yend"++_|_], Acc) -> lists:reverse(Acc);
-parse_body([Line|Rest], Acc) ->
-    parse_body(Rest, [Line|Acc]).
+parse_body(Lines)                   -> parse_body(Lines, []).
+parse_body([],Acc)                 -> lists:reverse(Acc);
+parse_body(["=yend"++_|_], Acc)     -> lists:reverse(Acc);
+parse_body(["=ypart"++_|Rest], Acc) -> parse_body(Rest, Acc);
+parse_body([Line|Rest], Acc)        -> parse_body(Rest, [Line|Acc]).
     
 decode(List) when is_list(List) -> decode(list_to_binary(List));
 decode(<<$=, X, Rest/binary>>)  -> <<(X-106):8, (decode(Rest))/binary>>;
@@ -29,9 +50,8 @@ decode_test_() ->
     [?_assertEqual(<<"Hello World">>, decode(<<114,143,150,150,153,74,129,153,156,150,142>>)),
      ?_assertEqual(<<19>>,            decode(<<61, 125>>))].
 
-parse_data_bin_test() ->
-    ?assertEqual({ok, [], <<"Hello World">>}, parse_data("", [[114,143,150,150,153,74,129,153,156,150,142], "=yend"])).
-
-parse_data_header_test_() ->
-    [?_assertEqual({ok, [{"foo", "bar"}], <<>>}, parse_data("foo=bar", [[],"=yend"])),
-     ?_assertEqual({ok, [{"foo", "bar"}, {"file", "foobar.txt"}], <<>>}, parse_data("foo=bar file=foobar.txt", [[],"=yend"]))].
+parse_test_() ->
+     ?_assertEqual({ok,
+                    [{"name", "foobar with spaces.txt"}, {"part", "1"}, {"size", "123"}, {"begin", "1"}, {"end", "11250"}],
+                    <<>>},
+                   parse("part=1 size=123 name=foobar with spaces.txt", ["=ypart begin=1 end=11250",[],"=yend"])).
