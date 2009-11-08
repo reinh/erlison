43294c630495c4a35a97aa396d4510a8e730dc34
Rein Henrichs <reinh@reinh.com>
2009-11-08 02:54:59 -0800

    Removing groups from nzb return values

--- src/nzb.erl
+++ src/nzb.erl
@@ -8,9 +8,8 @@ parse(NZB) ->
     FileElement = lists:last(xmerl_xpath:string("//file", Xml)),
     {file, Attributes, _} = xmerl_lib:simplify_element(FileElement),
     Subject    = proplists:get_value(subject, Attributes),
-    Groups     = get_text("//file/groups/group/text()", Xml),
     MessageIds = get_text("//file/segments/segment/text()", Xml),
-    {ok, Subject, Groups, MessageIds}.
+    {ok, Subject, MessageIds}.
 
 get_text(XpathString, Xml) ->
     [ Text || #xmlText{value=Text} <- xmerl_xpath:string(XpathString, Xml) ].
@@ -19,9 +18,7 @@ get_text(XpathString, Xml) ->
 
 parse_nzb_test() ->
     {ok, Xml} = file:read_file("../test/data/nzb.xml"),
-    {ok, Subject, Groups, MessageIds} = parse(Xml),
+    {ok, Subject, MessageIds} = parse(Xml),
     ?assertEqual(Subject, "Here's your file!  abc-mr2a.r01 (1/2)"),
-    ?assertEqual(Groups, ["alt.binaries.newzbin",
-                          "alt.binaries.mojo"]),
     ?assertEqual(MessageIds, ["123456789abcdef@news.newzbin.com",
                               "987654321fedbca@news.newzbin.com"]).
